6f6beea02407f50bea80d62dbbe2be9331056f0f
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2020-10-19 10:08:08 +0200

    Adding Firebase Firestore tests.

--- Serverless/WebVanilla/script.js
+++ Serverless/WebVanilla/script.js
@@ -1,13 +1,10 @@
+/**
+ * Prueba Firebase Auth
+ */
 /** Desde firebaseAuth.js */
 // user : Objeto usuario de FirebaseAuth cuando un usuario ha iniciado sesión o null cuando no.
 // login : Función para abrir el diálogo de inicio de sesión de Google.
 // logout : Función para cerrar la sesión de usuario.
-
-/** Desde firebaseFirestore.js */
-// firebaseDB : Objeto para acceder a la base de datos Firebase Firestore.
-
-/** Desde firebaseStorage.js */
-//firebaseStorageRoot : Objeto para acceder a la raiz del almacenamiento de Firebase Storage.
 
 const sessionButton = document.querySelector('#sessionButton');
 sessionButton.addEventListener("click",toggleSession);
@@ -30,6 +27,9 @@ function toggleSession () {
 /**
  * Prueba Firebase Storage
  */
+
+/** Desde firebaseStorage.js */
+//firebaseStorageRoot : Objeto para acceder a la raiz del almacenamiento de Firebase Storage.
 
 const uploadsFolder = firebaseStorageRoot.child('/webUploads/');
 
@@ -66,6 +66,7 @@ async function listFiles () {
                 var listElement = document.createElement('li');
                 var link = document.createElement('a');
                 link.href=url;
+                link.target="_blank"
                 link.innerText=name;
                 listElement.appendChild(link);
                 uploadsList.appendChild(listElement);
@@ -74,3 +75,45 @@ async function listFiles () {
 };
 
 listFiles();
+
+/**
+ * Prueba Firebase Firestore
+ */
+/** Desde firebaseFirestore.js */
+// firebaseDB : Objeto para acceder a la base de datos Firebase Firestore.
+
+const collectionPrints = firebaseDB.collection('prints');
+
+const printsTable = document.querySelector('#printsTable');
+
+async function showPrints() {
+    var querySnapshot = await collectionPrints.orderBy('date', 'desc').limit(10).get()
+    printsTable.innerText = '';
+    querySnapshot.forEach(
+        docReference => {
+            var doc = docReference.data();
+            var dateString = new Date(doc.date).toLocaleString()
+            var userAgent = doc.userAgent;
+            var tableRow = document.createElement('tr');
+            var tableCol1 = document.createElement('td');
+            var tableCol2 = document.createElement('td');
+            tableCol1.innerText = dateString;
+            tableCol2.innerText = userAgent;
+            tableRow.appendChild(tableCol1);
+            tableRow.appendChild(tableCol2);
+            printsTable.appendChild(tableRow);
+        }
+    );
+}
+
+async function registerPrint() {
+    var doc = {
+        userAgent: navigator.userAgent,
+        date: Date.now(),
+    }
+    await collectionPrints.add(doc);
+    showPrints();
+}
+
+registerPrint();
+
